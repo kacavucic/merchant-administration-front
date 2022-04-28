acb1d4c100590c1e395de170413dd573fb4c296e
Katarina Vucic <vucic.kat@gmail.com>
2022-04-28 17:53:21 +0200

    implemented login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/LoginPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
+  const [token, setToken] = useState();
+  function addToken(authToken) {
+    setToken(authToken);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        <Route path='/login' element={<LoginPage addToken={addToken} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = ({ addToken }) => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleInput(e) {
+    let newUserData = userData;
+    newUserData[e.target.name] = e.target.value;
+    setUserData(newUserData);
+  }
+
+  let navigate = useNavigate();
+  const [loginError, setLoginError] = useState(false);
+
+  function handleLogin(e) {
+    e.preventDefault();
+    axios
+      .post("api/login", userData)
+      .then((res) => {
+        console.log(res.data);
+        window.sessionStorage.setItem("auth_token", res.data.access_token);
+        addToken(res.data.access_token);
+        setLoginError(false);
+        navigate("/");
+      })
+      .catch((e) => {
+        setLoginError(true);
+        console.log(e);
+      });
+  }
+  return (
+    <section>
+      <div className="dark-overlay">
+        <div className="home-inner">
+          <div className="container">
+            <div className="row">
+              <div className="card col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+                <div className="card-body">
+                  <h4>Log In</h4>
+                  <hr></hr>
+                  {loginError === false ? (
+                    <></>
+                  ) : (
+                    <p className="alert alert-danger">
+                      Invalid email or password
+                    </p>
+                  )}
+
+                  <form onSubmit={handleLogin}>
+                    <div className="form-group">
+                      <label className="lead" htmlFor="email">
+                        Email
+                      </label>
+                      <input
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        placeholder="Enter email"
+                        type="text"
+                        onInput={handleInput}
+                      ></input>
+                    </div>
+                    <div className="form-group">
+                      <label className="lead" htmlFor="password">
+                        Password
+                      </label>
+                      <input
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        placeholder="Enter password"
+                        type="password"
+                        onInput={handleInput}
+                      ></input>
+                    </div>
+                    <button className="btn btn-primary" type="submit">
+                      Log In
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <p className="text-center">
+              Don't have an account? <a href="/register">Register here</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
